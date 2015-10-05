820483b517b4cb3a6872fd9a8481f119fa188a02
GeorgeID <gd.soft@mail.ru>
2015-10-06 01:24:42 +0300

    Создание конструктора Point

--- SnakeCons_SG/SnakeCons_SG/Point.cs
+++ SnakeCons_SG/SnakeCons_SG/Point.cs
@@ -12,6 +12,18 @@ namespace SnakeCons_SG
         public int y;
         public char sym;
 
+        // конструктор
+        public Point()
+        {
+        }
+        // конструктор
+        public Point(int _x, int _y, char _sym)
+        {
+            x = _x;
+            y = _y;
+            sym = _sym;
+        }
+        // метод
         public void Draw()
         {
             Console.SetCursorPosition(x, y);
